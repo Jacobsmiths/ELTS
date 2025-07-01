34cf337ffff26d889e4d3097f917e0de8295f70f
Jacob Smith <jaco.smith589@gmail.com>
2025-07-01 18:30:19 -0500

    added changes

--- ServoTesting.py
+++ ServoTesting.py
@@ -1,5 +1,6 @@
-import timer
 from gpiozero import Servo
+from threading import Event, Thread
+import time 
 from gpiozero.pins.pigpio import PiGPIOFactory
 
 quitApplication = False
@@ -7,9 +8,9 @@ quitApplication = False
 # these are the GPIO pins for the servos (To do: change these to your actual GPIO pins)
 xServoPin = 12
 
-Servo.pin_factory=PiGPIOFactory('127.0.0.1')
+Servo.pin_factory=PiGPIOFactory()
 # we will be using gpiozeros servo class to control the servos with PWM
-servo = Servo(pin =xServoPin, min_pulse_width=0.0005, max_pulse_width=0.0025)  # min and max pulse widths are found on the servos datasheet
+servo = Servo(pin =xServoPin, min_pulse_width=0.0005, max_pulse_width=0.0027, frame_width=0.02)  # min and max pulse widths are found on the servos datasheet
 servo.min()
 
 positions = ["min", "mid" , "max"]
@@ -21,13 +22,14 @@ class MyThread(Thread):
         self.stopped = event
 
     def switchPos(self):
-        current = (current+1)%3
-        if(positions[current]=="min"):
-            servo.min()
-        elif(posiitons[current]=="mid"):
-            servo.mid()
-        else:
-            servo.max()
+        servo.min()
+        print("min")
+        time.sleep(5)
+        servo.mid()
+        print("mid")
+        time.sleep(5)
+        servo.value = 1
+        print("max")
 
     def run(self):
         while not self.stopped.wait(3):
