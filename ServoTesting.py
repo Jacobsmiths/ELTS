43eba83f41ddf2eb603ccba2cc2f2509227f3706
Jacob Smith <jaco.smith589@gmail.com>
2025-07-01 15:40:45 -0500

    this test is slighlty working

--- ServoTesting.py
+++ ServoTesting.py
@@ -1,34 +1,38 @@
 import tkinter as tk
 from gpiozero import Servo
+from gpiozero.pins.pigpio import PiGPIOFactory
 
 quitApplication = False
 
 # these are the GPIO pins for the servos (To do: change these to your actual GPIO pins)
 xServoPin = 12
 
+Servo.pin_factory=PiGPIOFactory('127.0.0.1')
 # we will be using gpiozeros servo class to control the servos with PWM
-servo = Servo(xServoPin, min_pulse_width=0.0005, max_pulse_width=0.0025)  # min and max pulse widths are found on the servos datasheet
+servo = Servo(pin =xServoPin, min_pulse_width=0.0005, max_pulse_width=0.0025)  # min and max pulse widths are found on the servos datasheet
 servo.min()
 
-pos = IntVar(servo.val)
+# pos = tk.IntVar(value=servo.value)
+
+
+def setServoUp():
+    servo.max()
+    # pos+=0.1
+
+def setServoDown():
+    servo.min()
+       # pos-=0.01
+
 
 root = tk.Tk()
 root.title("Tester") 
-
-def setServoUp():
-    servo.value += 1
-    pos+=1
-
-def setServoDown():
-    servo.value-=1
-    pos-=1
-
-
 up_button = tk.Button(root, text="Up", command=setServoUp)
 up_button.pack(pady=10)
 
 down_button = tk.Button(root, text="Down", command=setServoDown)
 down_button.pack(pady=10)
 
-servo_pos = tk.Label(root, text=pos)
+# servo_pos = tk.Label(root, text=pos)
 
+
+root.mainloop()
