248af25401fffb626192facce7e0b900d3b50e96
Jacob Smith <jaco.smith589@gmail.com>
2025-08-24 14:14:02 -0500

    stuff

--- feedforward.py
+++ feedforward.py
@@ -232,7 +232,7 @@ if __name__ == "__main__":
     trackingThread.start()
     
     # Initialize and Start GUI in main thread
-    gui = GUI()
+    gui = GUI(startCommand=elts.startTracking, stopCommand=elts.stopTracking)
     gui.start()
     
     # Clean up once gui is closed
